07d0d3a9e3c73df85ec186e24f4c2c17a114c154
antoniocs <antoniocs@gmail.com>
2014-05-19 22:31:03 +0100

    Added some lessons

--- main.c
+++ main.c
@@ -10,8 +10,8 @@
 SDL_Window *gWindow = NULL;
 SDL_Surface *gScreenSurface = NULL;
 SDL_Surface *gXOut = NULL;
-SDL_Renderer* gRenderer = NULL;
-SDL_Texture* gTexture = NULL;
+SDL_Renderer *gRenderer = NULL;
+SDL_Texture *gTexture = NULL;
 TTF_Font *gFont = NULL;
 
 Uint32 sdl_unused_flags = 0;
@@ -20,6 +20,12 @@ int main(int argc, char** argv) {
     //lesson01(); //In this tutorial we will be setting up the SDL library and creating our first window.
     //lesson02(); //Now that we can get a window to appear, let blit an image onto it.
     //lesson03(); //Here's we'll start handling user input by allow the user to X out the window. - Game Loop
+    //lesson04(); //Here we'll learn to handle keyboard input.
+    //lesson05(); //Optimized Surface Loading and Soft Stretching
+    //lesson06(); //Extension Libraries and Loading Other Image Formats
+    //lesson07(); //Texture Loading and Rendering
+    //lesson08(); //Geometry Rendering
+    lesson09(); //The Viewport
 
     return (EXIT_SUCCESS);
 }
