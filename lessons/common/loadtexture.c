e1c590713ba34ffb935504c76cd97e7551358b0d
antoniocs <antoniocs@gmail.com>
2015-06-02 09:10:06 +0100

    Cleanup

--- lessons/common/loadtexture.c
+++ lessons/common/loadtexture.c
@@ -12,7 +12,6 @@ SDL_Texture *loadTexture(SDL_Surface **loadedSurface, SDL_Renderer *renderer) {
             //Get rid of old loaded surface
             SDL_FreeSurface(*loadedSurface);
         }
-
     }
 
     return newTexture;
