8524fd908382d61785666206067ca83dc10707be
antoniocs <antoniocs@gmail.com>
2015-06-07 23:24:45 +0100

    Basic template for a SDL (at least for now)

---
+++ basic_window_with_sdlimage.c
@@ -0,0 +1,55 @@
+#include <SDL.h>
+#include <SDL_Image.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "common/init_renderer.h"
+#include "common/init_only_sdlimage.h"
+#include "common/resource_path.h"
+
+int main(int argc, char** argv) {
+    const int SCREEN_WIDTH = 640;
+    const int SCREEN_HEIGHT = 480;
+
+    SDL_Surface *image = NULL;
+    SDL_Renderer *gRenderer = NULL;
+    SDL_Window *gWindow = init_renderer(&gRenderer);
+
+
+    if (gWindow != NULL && init_sdlimage()) {
+        bool quit = false;
+
+        //Event handler
+        SDL_Event e;
+
+        //While application is running
+        while (!quit) {
+            //Handle events on queue
+            while (SDL_PollEvent(&e) != 0) {
+                //User requests quit ----------- Quit also with ESCAPE key pressed
+                if (e.type == SDL_QUIT || (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE)) {
+                    quit = true;
+                }
+            }
+            
+            //Clear screen
+            SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+            SDL_RenderClear(gRenderer);
+
+            //Update screen
+            SDL_RenderPresent(gRenderer);
+        }
+    }
+
+    //Destroy window
+    SDL_DestroyRenderer(gRenderer);
+    gRenderer = NULL;
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL;
+
+    //Quit SDL subsystems
+    IMG_Quit();
+    SDL_Quit();
+
+    exit(EXIT_SUCCESS);
+}
