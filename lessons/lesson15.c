1bc585840ef6f2ae534620e8ac22d3dfad89313b
AntonioCS <antoniocs@gmail.com>
2015-06-11 17:27:03 +0100

    Finalized lesson 15

--- lessons/lesson15.c
+++ lessons/lesson15.c
@@ -13,12 +13,12 @@
 #include "common/init_renderer.h"
 #include "common/init_only_sdlimage.h"
 #include "common/resource_path.h"
+#include "class/LTexture.class.h"
 
 int main(int argc, char** argv) {
     const int SCREEN_WIDTH = 640;
     const int SCREEN_HEIGHT = 480;
 
-    SDL_Surface *image = NULL;
     SDL_Renderer *gRenderer = NULL;
     SDL_Window *gWindow = init_renderer(&gRenderer);
 
@@ -28,28 +28,68 @@ int main(int argc, char** argv) {
         //Event handler
         SDL_Event e;
 
-        //While application is running
-        while (!quit) {
-            //Handle events on queue
-            while (SDL_PollEvent(&e) != 0) {
-                //User requests quit ----------- Quit also with ESCAPE key pressed
-                if (e.type == SDL_QUIT || (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE)) {
-                    quit = true;
+        //Scene texture
+        LTexture *gArrowTexture = LTexture_Init(gRenderer);
+
+        if (gArrowTexture == NULL ||
+                !gArrowTexture->loadFromFile(gArrowTexture, resource_path("images/lesson15/arrow.png"))) {
+            printf("Unable to allocate ltexture or image\n");
+
+        } else {
+
+            //Angle of rotation
+            double degrees = 0;
+
+            //Flip type
+            SDL_RendererFlip flipType = SDL_FLIP_NONE;
+
+            //While application is running
+            while (!quit) {
+                //Handle events on queue
+                while (SDL_PollEvent(&e) != 0) {
+                    //User requests quit ----------- Quit also with ESCAPE key pressed
+                    if (e.type == SDL_QUIT || (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE)) {
+                        quit = true;
+                    } else if (e.type == SDL_KEYDOWN) {
+                        switch (e.key.keysym.sym) {
+                            case SDLK_a:
+                                degrees -= 60;
+                                break;
+
+                            case SDLK_d:
+                                degrees += 60;
+                                break;
+
+                            case SDLK_q:
+                                flipType = SDL_FLIP_HORIZONTAL;
+                                break;
+
+                            case SDLK_w:
+                                flipType = SDL_FLIP_NONE;
+                                break;
+
+                            case SDLK_e:
+                                flipType = SDL_FLIP_VERTICAL;
+                                break;
+                        }
+                    }
                 }
+
+                //Clear screen
+                SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+                SDL_RenderClear(gRenderer);
+
+                //Render arrow
+                gArrowTexture->renderAngle(gArrowTexture, (SCREEN_WIDTH - gArrowTexture->getWidth(gArrowTexture)) / 2, (SCREEN_HEIGHT - gArrowTexture->getHeight(gArrowTexture)) / 2, NULL, degrees, NULL, flipType);
+
+                //Update screen
+                SDL_RenderPresent(gRenderer);
+
+                //So its not super cpu intensive
+                SDL_Delay(1);
             }
-
-            //Clear screen
-            SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-            SDL_RenderClear(gRenderer);
-
-            //Update screen
-            SDL_RenderPresent(gRenderer);
-
-            //So its not super cpu intensive
-            SDL_Delay(1);
         }
-    }
-    else {
+    } else {
         printf("Something wrong with the window: %s\n", SDL_GetError());
         exit(EXIT_FAILURE);
     }
