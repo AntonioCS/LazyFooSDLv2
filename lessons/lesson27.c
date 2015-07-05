64f2c080086ac8fb142aad4a40802f2de4748072
antoniocs <antoniocs@gmail.com>
2015-07-05 23:17:09 +0100

    Started lesson 27

---
+++ lessons/lesson27.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   lesson27.c
+ * Author: antoniocs
+ *
+ * Created on 2 de Julho de 2015, 8:09
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
